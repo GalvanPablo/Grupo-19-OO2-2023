7fc629e0f5fe6f22c0820bb22d760ea685fc507c
Pablo Galvan <pablogalvan.015@gmail.com>
2023-05-27 22:19:53 -0300

    1er commit

---
+++ src/main/java/com/unla/grupo19/grupo19OO22023/utils/RouteHelper.java
@@ -0,0 +1,6 @@
+package com.unla.grupo19.grupo19OO22023.utils;
+
+public class RouteHelper {
+	
+	// public final static String INDEX = "home/index";
+}
---
+++ src/test/java/com/unla/grupo19/grupo19OO22023/Grupo19Oo22023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unla.grupo19.grupo19OO22023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo19Oo22023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
